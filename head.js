8ef77136d2ca0e02860151a556098798b7561eb5
Samicap <lovestochasewindmills@gmail.com>
2020-11-23 21:46:58 +0000

    added head.js

---
+++ head.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`💚 Assertion Passed: ${actual}  ===  ${expected}`);
+  } else {
+    console.log(`💔 Assertion Failed: ${actual} !== ${expected}`);
+
+  }
+};
+
+let array = [];
+
+const head = function(array) {
+  return array[0]
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([], []));
+assertEqual(head(["one"], "five"))
+
