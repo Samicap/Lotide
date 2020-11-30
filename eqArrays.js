caf3cea19ff161b0bef1f6c7d5a137e2aaa2de0b
Samicap <lovestochasewindmills@gmail.com>
2020-11-30 20:04:57 +0000

    added module.export to eqArrays.js and removed assertEqual to seperate file.

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`💚 Assertion Passed: ${actual}  ===  ${expected}`);
-  } else {
-    console.log(`💔 Assertion Failed: ${actual} !== ${expected}`);
 
-  }
-};
+const assertEqual = require('./assertEqual'); //this doesnt do anything. possibly need to take out.
 
 // Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
 let eqArrays = function(a, b) {
@@ -22,6 +16,8 @@ let eqArrays = function(a, b) {
   }
 };
 
+module.exports = eqArrays;
+
 // Old code for eqArrays.  Didn't seem to work once i got to eqObjects due to the Arrary.isArray
 // let eqArrays = function(a, b) {
 //   return Array.isArray(a) &&
@@ -30,8 +26,8 @@ let eqArrays = function(a, b) {
 //     a.every((val, index) => val === b[index]);
 // };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), true);
-assertEqual(eqArrays([1, 3, 4], [5, 6, 7]), false);
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), true);
+// assertEqual(eqArrays([1, 3, 4], [5, 6, 7]), false);
 
 //another way to check if arrays are equal, however this JSON doesnt work for objects. So JSON works for these small test cases but isn't good for the real word.
 //JSON only checks primitive data types. It wont check for "deeper values" like those in objects.
