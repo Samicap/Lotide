33e9c23cc4fa740a6b7cbe75444fbe0964fd6820
Samicap <lovestochasewindmills@gmail.com>
2020-11-30 20:26:40 +0000

    added assertArraysEqualTests.js to test assertArraysEqual function

---
+++ test/assertArraysEqualTests.js
@@ -0,0 +1,7 @@
+const assertEqual = require('../assertEqual');
+const assertArraysEqual = require('../assertArraysEqual');
+
+
+assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
+assertArraysEqual([1, 3, 4], [5, 6, 7]);
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
