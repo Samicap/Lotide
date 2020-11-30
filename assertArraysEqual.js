891d9b5a831fa77f09c99a9f6dbe5c59523c2444
Samicap <lovestochasewindmills@gmail.com>
2020-11-30 20:25:57 +0000

    added module and require eqArrays on assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,18 +1,21 @@
+const eqArrays = require('./eqArrays');
+
+
 const assertArraysEqual = function(actual, expected) {
-  if (arrayEquals(actual, expected)) {
+  if (eqArrays(actual, expected)) {
     console.log(`💚 Assertion Passed: ${actual}  ===  ${expected}`);
   } else {
     console.log(`💔 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-let arrayEquals = function(a, b) {
-  return Array.isArray(a) &&
-    Array.isArray(b) &&
-    a.length === b.length &&
-    a.every((val, index) => val === b[index]);
-};
+module.exports = assertArraysEqual;
 
-assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
-assertArraysEqual([1, 3, 4], [5, 6, 7]);
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
+
+
+// let arrayEquals = function(a, b) {
+//   return Array.isArray(a) &&
+//     Array.isArray(b) &&
+//     a.length === b.length &&
+//     a.every((val, index) => val === b[index]);
+// };
